8d1276af911a8b373a88c03f397d5283e4cdbcde
Brian Donnoe <dunow33@hotmail.com>
2021-10-25 19:34:20 -0400

    first commit

---
+++ app/src/main/java/com/example/mvvmnewsapp/NewsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mvvmnewsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        //bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+}
---
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = " https://api.giphy.com/ "
+        const val API_KEY = "GHTLkBl2GW09fjdGUnPz6SU1Pa1UP6si"
+        const val TIMEOUT = 60 * 1000L
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
